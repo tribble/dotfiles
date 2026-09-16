10c3d636b8bd5650d28ba6ebcb91285a2e370c2d
tribble <tribble@users.noreply.github.com>
2026-09-16 12:18:39 -0700

    sync-back: drop aws.fish; ~/.aws/config is a symlink to infra's native SSO config

--- scripts/sync-back.sh
+++ scripts/sync-back.sh
@@ -13,7 +13,6 @@ paths=(
   config/herdr/config.toml
   config/fish/conf.d/ws.fish
   config/fish/conf.d/pr.fish
-  config/fish/conf.d/aws.fish
   config/fish/conf.d/fish_frozen_theme.fish
   config/fish/fish_plugins
   config/starship.toml
